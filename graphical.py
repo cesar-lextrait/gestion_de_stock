6b5aee6849baa16a48f3d07194b2260c60c95d02
cesar-lextrait <cesar.lextrait@laplateforme.io>
2023-03-18 09:58:48 +0100

    rajout de petites fonctions et boutons, graphiques basiques

--- graphical.py
+++ graphical.py
@@ -2,12 +2,22 @@ import mysql.connector
 import tkinter as tk
 from tkinter import ttk
 from main import *
+from PIL import Image, ImageTk
 
 
 # Créer la fenêtre principale
 root = tk.Tk()
-root.title("Gestion de stock")
-root.geometry("600x400")
+root.title("Stock Librairie")
+root.geometry("1200x700")
+
+image = Image.open("/home/cezcew/Bureau/Ecole/gestion_de_stock/book.png")
+image = image.resize((200, 200), Image.ANTIALIAS)
+photo = ImageTk.PhotoImage(image)
+
+frame_photo = tk.Frame(root)
+frame_photo.pack(side=tk.TOP, padx=10, pady=10)
+label_photo = tk.Label(frame_photo, image=photo)
+label_photo.pack()
 
 # Créer les sous-frames pour les boutons de gauche et de droite
 frame_left = tk.Frame(root)
@@ -15,29 +25,40 @@ frame_right = tk.Frame(root)
 frame_left.pack(side=tk.LEFT, padx=10, pady=10)
 frame_right.pack(side=tk.RIGHT, padx=10, pady=10)
 
+frame_south =tk.Frame(root)
+frame_south.pack(side=tk.TOP, padx=10, pady=20)
+frame_south2 =tk.Frame(root)
+frame_south2.pack(side=tk.TOP, padx=10, pady=20 )
+
 # Créer les sous-frames pour chaque bouton
 frame_add = tk.Frame(frame_left)
 frame_modif = tk.Frame(frame_left)
 frame_supp = tk.Frame(frame_right)
 frame_afficher = tk.Frame(frame_right)
+frame_categories = tk.Frame(frame_south)
+frame_add_categories = tk.Frame(frame_south2)
 
 # Ajouter les sous-frames dans la grille pour qu'elles soient bien rangées
 frame_add.grid(row=0, column=0, padx=10, pady=10)
 frame_modif.grid(row=1, column=0, padx=10, pady=10)
 frame_supp.grid(row=0, column=0, padx=10, pady=10)
 frame_afficher.grid(row=1, column=0, padx=10, pady=10)
-
+frame_categories.grid(row=2, column=0, padx=10, pady=10)
+frame_add_categories.grid(row=3, column=0, padx=10, pady=10)
 # Créer les boutons et les ajouter dans les sous-frames correspondantes
 btn_add = tk.Button(frame_add, text="Ajouter un produit", command=Ajouter.ajouter_produit, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
 btn_modif = tk.Button(frame_modif, text="Modifier un produit", command=Modif.modif_produits, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
-btn_supp = tk.Button(frame_supp, text="Supprimer un produit", command=Afficher.afficher_produits, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
+btn_supp = tk.Button(frame_supp, text="Supprimer un produit", command=Supprimer.supprimer_produit, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
 btn_afficher = tk.Button(frame_afficher, text="Afficher les produits", command=Afficher.afficher_produits, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
-
+btn_categories = tk.Button(frame_categories, text="Afficher les catégories",command=Afficher_Categorie.afficher_categorie, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
+btn_categories2 = tk.Button(frame_add_categories, text="Ajouter une catégorie",command=Ajouter_Categorie.ajouter_categorie, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
 # Ajouter les boutons dans les sous-frames correspondantes
 btn_add.pack(expand=True, fill="both")
 btn_modif.pack(expand=True, fill="both")
 btn_supp.pack(expand=True, fill="both")
 btn_afficher.pack(expand=True, fill="both")
+btn_categories.pack(expand=True, fill="both")
+btn_categories2.pack(expand=True, fill="both")
 
 
 
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import mysql.connector
 import mysql.connector
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+from tkinter.tix import *
 import graphical
+
+
 
 def fetch_data():
     db = mysql.connector.connect(
@@ -18,13 +22,26 @@ def fetch_data():
     result = cur.fetchall()
     return result
 
+def fetch_data_categorie():
+    categorie_db = mysql.connector.connect(
+            host="localhost",
+            user="gestion_stock",
+            password="abcde12345;ABCDE",
+            database="Boutique"
+    )
+    cur = categorie_db.cursor()
+    sql = ("SELECT * FROM categorie")
+    cur.execute(sql)
+    result2 = cur.fetchall()
+    return result2
+
 class Afficher:
     def afficher_produits():
         # Créer la fenêtre pour afficher les produits
         afficher = tk.Toplevel()
         afficher.title("Afficher les produits")
         afficher.geometry("1400x600")
-
+        
         # Créer la frame pour la liste des produits
         treeview = ttk.Treeview(afficher, columns=(1, 2, 3, 4, 5), show="headings", height="6")
         treeview.pack(expand=True, fill="both")
@@ -42,8 +59,61 @@ class Afficher:
 
         afficher.mainloop()
 
-     
 
+class Afficher_Categorie:
+
+    def afficher_categorie():
+        afficher_cat = tk.Toplevel()
+        afficher_cat.title("Catégories")
+        afficher_cat.geometry("1400x600")
+
+        treeview = ttk.Treeview(afficher_cat, columns=(1, 2), show="headings", height="2")
+        treeview.pack(expand=True, fill="both")
+
+        treeview.heading(1, text="ID")
+        treeview.heading(2, text="Nom")
+
+        data = fetch_data_categorie()
+
+        for row in data:
+            treeview.insert("", tk.END, values=(row[0], row[1]))
+        afficher_cat.mainloop()
+
+       
+
+
+class Ajouter_Categorie:
+    def ajouter_categorie():
+        ajouter_cat = tk.Toplevel()
+        ajouter_cat.title("Ajouter une catégorie")
+        ajouter_cat.geometry("1400x600")
+
+        frame_input = tk.Frame(ajouter_cat)
+        frame_input.pack(expand=True, fill="both")
+
+        label_nom = tk.Label(frame_input, text="Nom", font=("Arial", 15))
+        label_nom.pack(expand=True, fill="both")
+        entry_nom = tk.Entry(frame_input, font=("Arial", 15))
+        entry_nom.pack(expand=True, fill="both")
+
+        button_valider = tk.Button(frame_input, text="Valider", font=("Arial", 15), bg="black", fg="white",activebackground='green', width=20, height=2,
+                       command=lambda: valider_ajout_categorie(entry_nom.get()))
+        button_valider.pack(expand=True, fill="both")
+
+        def valider_ajout_categorie(nom):
+            db = mysql.connector.connect(
+                host="localhost",
+                user="gestion_stock",
+                password="abcde12345;ABCDE",
+                database="Boutique"
+            )
+            cur = db.cursor()
+            sql = f"INSERT INTO categorie (nom) VALUES ('{nom}')"
+            cur.execute(sql)
+            db.commit()
+            db.close()
+
+            messagebox.showinfo("Ajout", "Catégorie ajoutée avec succès !")
 
 class Modif:
     def modif_produits():
@@ -51,18 +121,14 @@ class Modif:
         modif = tk.Toplevel()
         modif.title("Modifier les produits")
         modif.geometry("800x600")
-
-        # Créer la frame pour la liste des produits
-        frame_liste = tk.Frame(modif)
-        frame_liste.pack(expand=True, fill="both")
-
+        
         # Créer la frame pour les boutons
         frame_btn = tk.Frame(modif)
         frame_btn.pack(expand=True, fill="both")
 
         # Créer les boutons
-        btn_modif_prix = tk.Button(frame_btn, text="Modifier le prix", command=Modif.modif_prix, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
-        btn_modif_quantite = tk.Button(frame_btn, text="Modifier la quantité", command=Modif.modif_quantite, font=("Arial", 15), bg="white", fg="black", width=20, height=2)
+        btn_modif_prix = tk.Button(frame_btn, text="Modifier le prix", command=Modif.modif_prix, font=("Arial", 30), bg="white", fg="black", width=20)
+        btn_modif_quantite = tk.Button(frame_btn, text="Modifier la quantité", command=Modif.modif_quantite, font=("Arial", 30), bg="white", fg="black", width=20)
         # Ajouter les boutons dans la frame
         btn_modif_prix.pack(expand=True, fill="both")
         btn_modif_quantite.pack(expand=True, fill="both")
@@ -71,7 +137,7 @@ class Modif:
     def modif_prix(): # Fonction pour modifier le prix
         afficher = tk.Toplevel()
         afficher.title("Modifier le prix")
-        afficher.geometry("800x600")
+        afficher.geometry("1400x600")
         frame_liste = tk.Frame(afficher)
         frame_liste.pack(expand=True, fill="both")
         data = fetch_data()
@@ -103,11 +169,11 @@ class Modif:
         id_produit = [row[0] for row in data]
         combobox_id_produit = ttk.Combobox(frame_input, values=id_produit, font=("Arial", 15))
         combobox_id_produit.pack(expand=True, fill="both")
-        button_valider = tk.Button(frame_input, text="Valider", font=("Arial", 15), bg="blue", fg="black", width=20, height=2,
+        button_valider = tk.Button(frame_input, text="Valider", font=("Arial", 15), bg="black", fg="white",activebackground='green', width=20, height=2,
                        command=lambda: valider_modif_prix(entry_nouveau_prix.get(), combobox_id_produit.get()))
         button_valider.pack(expand=True, fill="both")
-    
 
+      
         def valider_modif_prix(nouveaux_prix, id_produit):
                 db = mysql.connector.connect(
                     host="localhost",
@@ -120,18 +186,25 @@ class Modif:
                 print(sql)
                 cur.execute(sql)
                 db.commit()
+                treeview.delete(*treeview.get_children())
+                sql = "SELECT * FROM produit"
+                cur.execute(sql)
+                rows = cur.fetchall()
                 db.close()
-                afficher.destroy()
-
+                for row in rows:
+                    treeview.insert("", tk.END, values=(row[0], row[1], row[2], row[3], row[4]))
+                messagebox.showinfo("Succès", f"Le prix du produit {id_produit} a été modifié avec succès")
+                    
+               
 
 
 
     def modif_quantite():
         afficher = tk.Toplevel()
         afficher.title("Modifier le prix")
-        afficher.geometry("800x600")
+        afficher.geometry("1400x600")
         frame_liste = tk.Frame(afficher)
-        frame_liste.pack(expand=True, fill="both")
+        frame_liste.pack(expand=True, fill="both", anchor="n")
         data = fetch_data()
         # Créer la frame pour la liste des produits
         treeview = ttk.Treeview(afficher, columns=(1, 2, 3, 4, 5), show="headings", height="6")
@@ -150,21 +223,19 @@ class Modif:
 
         frame_input = tk.Frame(afficher)
         frame_input.pack(expand=True, fill="both")
-        frame_input = tk.Frame(afficher)
-        frame_input.pack(expand=True, fill="both")
 
-        label_nouvelle_quantite = tk.Label(frame_input, text="Nouvelle quantité", font=("Arial", 15))
+        label_nouvelle_quantite = tk.Label(frame_input, text="Nouvelle quantité", font=("Arial", 14), width=20, height=2)
         label_nouvelle_quantite.pack(expand=True, fill="both")
-        entry_nouvelle_quantite = tk.Entry(frame_input, font=("Arial", 15))
+        entry_nouvelle_quantite = tk.Entry(frame_input, font=("Arial", 14))
         entry_nouvelle_quantite.pack(expand=True, fill="both")
 
-        label_id_produit = tk.Label(frame_input, text="ID du produit", font=("Arial", 15))
+        label_id_produit = tk.Label(frame_input, text="ID du produit", font=("Segoe Print", 14), width=20, height=2)
         label_id_produit.pack(expand=True, fill="both")
         id_produit = [row[0] for row in data]
-        combobox_id_produit = ttk.Combobox(frame_input, values=id_produit, font=("Arial", 15))
+        combobox_id_produit = ttk.Combobox(frame_input, values=id_produit, font=("Arial", 15), width=20, height=2)
         combobox_id_produit.pack(expand=True, fill="both")
 
-        button_valider = tk.Button(frame_input, text="Valider", font=("Arial", 15), bg="blue", fg="black", width=20, height=2,
+        button_valider = tk.Button(frame_input, text="Valider", font=("Arial", 15), bg="black", activebackground="green", fg="white", width=20, height=2,
                            command=lambda: valider_modif_quantite(entry_nouvelle_quantite.get(), combobox_id_produit.get()))
         button_valider.pack(expand=True, fill="both")
 
@@ -179,7 +250,14 @@ class Modif:
                 sql = f"UPDATE produit SET quantité = {nouvelle_quantite} WHERE id = {id_produit}"
                 cur.execute(sql)
                 db.commit()
+                treeview.delete(*treeview.get_children())
+                sql = "SELECT * FROM produit"
+                cur.execute(sql)
+                rows = cur.fetchall()
                 db.close()
+                for row in rows:
+                    treeview.insert("", tk.END, values=(row[0], row[1], row[2], row[3], row[4]))
+                messagebox.showinfo("Modification", "La modification a bien été prise en compte")
 
 
 class Ajouter:
@@ -231,3 +309,44 @@ class Ajouter:
             cur.execute(sql)
             db.commit()
             db.close()
+            messagebox.showinfo("Ajout", "Le produit a bien été ajouté")
+
+class Supprimer:
+    def supprimer_produit():
+        suppr = tk.Toplevel()
+        suppr.title("Supprimer un produit")
+        suppr.geometry("800x600")
+        frame_liste = tk.Frame(suppr)
+        frame_liste.pack(expand=True, fill="both")
+        data = fetch_data()
+        # Créer la frame pour la liste des produits
+        treeview = ttk.Treeview(suppr, columns=(1, 2, 3, 4, 5), show="headings", height="6")
+        treeview.pack(expand=True, fill="both")
+        for row in data:
+            treeview.insert("", tk.END, values=(row[0], row[1], row[2], row[3], row[4]))
+        frame_input = tk.Frame(suppr)
+        frame_input.pack(expand=True, fill="both")
+        label_id_produit = tk.Label(frame_input, text="ID du produit", font=("Arial", 15))
+        label_id_produit.pack(expand=True, fill="both")
+        id_produit = [row[0] for row in data]
+        combobox_id_produit = ttk.Combobox(frame_input, values=id_produit, font=("Arial", 15))
+        combobox_id_produit.pack(expand=True, fill="both")
+        button_valider = tk.Button(frame_input, text="Valider", font=("Arial", 15), bg="blue", fg="black", width=20, height=2,
+                                    command=lambda: valider_suppression(combobox_id_produit.get()))
+        button_valider.pack(expand=True, fill="both")
+
+        def valider_suppression(id_produit):
+            item = treeview.selection()[0]
+            id_produit = treeview.item(item)['values'][0]
+            db = mysql.connector.connect(
+                host="localhost",
+                user="gestion_stock",
+                password="abcde12345;ABCDE",
+                database="Boutique"
+            )   
+            cur = db.cursor()
+            sql = f"DELETE FROM produit WHERE id = {id_produit}"
+            cur.execute(sql)
+            db.commit()
+            db.close()
+            messagebox.showinfo("Suppression", "Le produit a bien été supprimé")
